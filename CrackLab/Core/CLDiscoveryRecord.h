3a3df7033252919af093adc941f39944f379efca
ZAIN <aosaid3224@gmail.com>
2026-09-21 04:51:41 +0300

    Discovery record: add iTunesMetadata genre fields

--- CrackLab/Core/CLDiscoveryRecord.h
+++ CrackLab/Core/CLDiscoveryRecord.h
@@ -8,14 +8,16 @@
 @interface CLDiscoveryRecord : NSObject
 @property (nonatomic, strong) NSString *bundleID;
 @property (nonatomic, strong) NSString *name;
-@property (nonatomic, strong) NSString *appType;      // مستخدم / نظام / أخرى
+@property (nonatomic, strong) NSString *appType;        // مستخدم / نظام / أخرى / غير معروف
 @property (nonatomic, strong) NSString *bundlePath;
 @property (nonatomic, assign) BOOL hasInfoPlist;
-@property (nonatomic, strong) NSString *categoryType; // LSApplicationCategoryType
+@property (nonatomic, strong) NSString *categoryType;   // LSApplicationCategoryType
 @property (nonatomic, strong) NSArray<NSString *> *genres;
 @property (nonatomic, strong) NSArray<NSNumber *> *genreIDs;
+@property (nonatomic, strong) NSNumber *iTunesGenreId;  // من iTunesMetadata.plist داخل الحزمة
+@property (nonatomic, strong) NSString *iTunesGenre;
 @property (nonatomic, strong) NSString *engineName;
-@property (nonatomic, strong) NSArray<NSString *> *signals;  // why Game / why not
+@property (nonatomic, strong) NSArray<NSString *> *signals;
 @property (nonatomic, assign) NSInteger score;
 @property (nonatomic, assign) BOOL isGame;
 - (NSDictionary *)dictionaryRepresentation;
